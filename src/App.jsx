1f2ef622b55319896f0f2f9114a18574ce25b87d
Gautam Bansal <gautamdevelops@gmail.com>
2024-01-25 14:13:17 +0530

    Application Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./app.scss";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Projects from "./components/projects/Projects";
 import Services from "./components/services/Services";
 
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
@@ -20,10 +24,10 @@ const App = () => {
       <section id="Projects">
         <Parallax type="projects" />
       </section>
-      <section>Project 1</section>
-      <section>Project 2</section>
-      <section>Project 3</section>
-      <section id="Contact">Contact</section>
+      <Projects />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,78 @@
+import { useRef } from "react";
+import "./projects.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Blog App",
+    img: "/blog.png",
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.",
+  },
+  {
+    id: 2,
+    title: "Blog App",
+    img: "/blog.png",
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.",
+  },
+  {
+    id: 3,
+    title: "Blog App",
+    img: "/blog.png",
+    desc: "lorem ipsum dolor sit amet consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Projects = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+  return (
+    <div className="projects" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
